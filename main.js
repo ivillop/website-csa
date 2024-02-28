7f1e4751d427b6f384b46f8f817d85f9ecd4a549
ivillop <rd.akbar@yandex.com>
2024-02-29 02:41:13 +0700

    create responsive website

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ faqItems.forEach((faqItem) => {
     faqItem.classList.toggle('active');
   });
 });
+setTimeout(() => {
+  document.body.classList.add('loaded'); // Add "loaded" class when page is loaded
+}, 500);
+
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar ul');
+  sidebar.style.transform = 'translateX(45%)';
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar ul');
+  sidebar.style.transform = 'translateX(100%)';
+}
